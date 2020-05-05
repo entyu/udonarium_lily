0964a50e4657b6f1fd41916420669649aedbc0e1
Nanasu NANA <nnanasu7@gmail.com>
2020-05-05 13:24:56 +0900

    BCDiceバージョンアップ対応。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -156,7 +156,7 @@ export class AppComponent implements AfterViewInit, OnDestroy {
                 .filter(info => info.system != 'DiceBot')
                 .map(info => {
                   // namesの結果にソートキー欲しい
-                  let normalize = (info.sortKey && info.sortKey.indexOf('国際化') < 0) ? info.sortKey : info.name.normalize('NFKD');
+                  let normalize = (info.sort_key && info.sort_key.indexOf('国際化') < 0) ? info.sort_key : info.name.normalize('NFKD');
                   for (let replaceData of DiceBot.replaceData) {
                     if (replaceData[2] && info.name === replaceData[0]) {
                       normalize = replaceData[1];
@@ -178,9 +178,7 @@ export class AppComponent implements AfterViewInit, OnDestroy {
                 })
                 .map(info => {
                   const lang = /.+\:(.+)/.exec(info.system);
-                  info.lang = lang ? lang[1] 
-                    : /^[0-9a-zA-Z]+$/.test(info.normalize) ? 'B'
-                    : 'A';
+                  info.lang = lang ? lang[1] : 'A';
                   return info;
                 })
                 .sort((a, b) => {
