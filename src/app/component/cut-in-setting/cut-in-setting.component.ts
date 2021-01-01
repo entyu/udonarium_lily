a6581c170e69d5676bcd0cbf03f7a1bc393f67a6
DESKTOP-64JNFCF\keyra <nnanasu7@gmail.com>
2021-01-02 01:14:09 +0900

    cherry-pick:カットイン設定コンポーネント。

---
+++ src/app/component/cut-in-setting/cut-in-setting.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cut-in-setting',
+  templateUrl: './cut-in-setting.component.html',
+  styleUrls: ['./cut-in-setting.component.css']
+})
+export class CutInSettingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  add() {
+    
+  }
+}
