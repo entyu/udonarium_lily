aa0ecb0b449d8e92d5b818fef4091863ddeac8fa
DESKTOP-64JNFCF\keyra <nnanasu7@gmail.com>
2021-05-18 13:35:54 +0900

    cherry-pick:カットイン画像のみだいたい。

---
+++ src/app/class/cut-in.ts
@@ -0,0 +1,32 @@
+import { ImageFile } from './core/file-storage/image-file';
+import { SyncObject, SyncVar } from './core/synchronize-object/decorator';
+import { ObjectNode } from './core/synchronize-object/object-node';
+
+@SyncObject('cut-in')
+export class CutIn extends ObjectNode {
+  @SyncVar() name: string = '';
+  @SyncVar() duration: number = 6;
+
+  @SyncVar() width: number = 30;
+  @SyncVar() height: number = 0;
+  @SyncVar() posX: number = 50;
+  @SyncVar() posY: number = 50;
+  @SyncVar() zIndex: number = 0;
+  @SyncVar() isFrontOfStand: boolean = false;
+  @SyncVar() imageIdentifier: string = ImageFile.Empty.identifier;
+
+  @SyncVar() audioName: string = '';
+  @SyncVar() startTime: number = 0;
+  @SyncVar() tagName: string = '';
+  @SyncVar() isLoop: boolean = false;
+  @SyncVar() isPlaying: boolean = false;
+  @SyncVar() audioIdentifier: string = '';
+  @SyncVar() tag: string = '';
+
+  get conditionTexts(): string[] {
+    if (!this.value) return [];
+    return (<string>this.value).split(/[\r\n]+/).map(row => {
+      return row ? '' : row.trim();
+    });
+  }
+}
--- src/app/component/cut-in-setting/cut-in-setting.component.ts
+++ src/app/component/cut-in-setting/cut-in-setting.component.ts
@@ -1,18 +1,255 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, NgZone, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { CutInList } from '@udonarium/cut-in-list';
+import { CutIn } from '@udonarium/cut-in';
+import { ObjectStore } from '@udonarium/core/synchronize-object/object-store';
+import { ObjectSerializer } from '@udonarium/core/synchronize-object/object-serializer';
+import { PointerDeviceService } from 'service/pointer-device.service';
+import { ModalService } from 'service/modal.service';
+import { PanelOption, PanelService } from 'service/panel.service';
+import { SaveDataService } from 'service/save-data.service';
+import { EventSystem } from '@udonarium/core/system';
+import { TextViewComponent } from 'component/text-view/text-view.component';
+import { ImageFile } from '@udonarium/core/file-storage/image-file';
+import { ImageStorage } from '@udonarium/core/file-storage/image-storage';
+import { ImageTag } from '@udonarium/image-tag';
+import { FileSelecterComponent } from 'component/file-selecter/file-selecter.component';
+import { CutInService } from 'service/cut-in.service';
+
 
 @Component({
   selector: 'app-cut-in-setting',
   templateUrl: './cut-in-setting.component.html',
   styleUrls: ['./cut-in-setting.component.css']
 })
-export class CutInSettingComponent implements OnInit {
+export class CutInSettingComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('cutInSelecter') cutInSelecter: ElementRef<HTMLSelectElement>;
+  readonly minSize: number = 0;
+  readonly maxSize: number = 100;
 
-  constructor() { }
+  isShowHideImages = false;
+  selectedCutIn: CutIn = null;
+  selectedCutInXml: string = '';
+
+  get cutIns(): CutIn[] { return CutInList.instance.cutIns; }
+
+  get cutInName(): string { return this.selectedCutIn.name; }
+  set cutInName(cutInName: string) { if (this.isEditable) this.selectedCutIn.name = cutInName; }
+
+  get cutInTag(): string { return this.selectedCutIn.tag; }
+  set cutInTag(cutInTag: string) { if (this.isEditable) this.selectedCutIn.tag = cutInTag; }
+
+  get cutInDuration(): number { return this.selectedCutIn.duration; }
+  set cutInDuration(cutInDuration: number) { if (this.isEditable) this.selectedCutIn.duration = cutInDuration; }
+
+  get cutInCond(): string { return this.selectedCutIn.value + ''; }
+  set cutInCond(cutInCond: string) { if (this.isEditable) this.selectedCutIn.value = cutInCond; }
+
+  get cutInWidth(): number { return this.selectedCutIn.width; }
+  set cutInWidth(cutInWidth: number) { if (this.isEditable) this.selectedCutIn.width = cutInWidth; }
+
+  get cutInHeight(): number { return this.selectedCutIn.height; }
+  set cutInHeight(cutInHeight: number) { if (this.isEditable) this.selectedCutIn.height = cutInHeight; }
+
+  get cutInPosX(): number { return this.selectedCutIn.posX; }
+  set cutInPosX(cutInPosX: number) { if (this.isEditable) this.selectedCutIn.posX = cutInPosX; }
+
+  get cutInPosY(): number { return this.selectedCutIn.posY; }
+  set cutInPosY(cutInPosY: number) { if (this.isEditable) this.selectedCutIn.posY = cutInPosY; }
+
+  get cutInZIndex(): number { return this.selectedCutIn.zIndex; }
+  set cutInZIndex(cutInZIndex: number) { if (this.isEditable) this.selectedCutIn.zIndex = cutInZIndex; }
+
+  get cutInIsFrontOdStand(): boolean { return this.selectedCutIn.isFrontOfStand; }
+  set cutInIsFrontOdStand(cutInIsFrontOdStand: boolean) { if (this.isEditable) this.selectedCutIn.isFrontOfStand = cutInIsFrontOdStand; }
+
+  get isEmpty(): boolean { return this.cutIns.length < 1; }
+  get isDeleted(): boolean { return this.selectedCutIn ? ObjectStore.instance.get(this.selectedCutIn.identifier) == null : false; }
+  get isEditable(): boolean { return !this.isEmpty && !this.isDeleted; }
+
+  get cutInImage(): ImageFile {
+    if (!this.selectedCutIn) return ImageFile.Empty;
+    let file = ImageStorage.instance.get(this.selectedCutIn.imageIdentifier);
+    return file ? file : ImageFile.Empty;
+  }
+  
+  get isNowShowing(): boolean {
+    if (!this.selectedCutIn) return false;
+    return CutInService.nowShowingIdentifiers().includes(this.selectedCutIn.identifier);
+  }
+  
+  isSaveing: boolean = false;
+  progresPercent: number = 0;
+
+  constructor(
+    private pointerDeviceService: PointerDeviceService,
+    private modalService: ModalService,
+    private panelService: PanelService,
+    private saveDataService: SaveDataService
+  ) { }
 
   ngOnInit(): void {
+    Promise.resolve().then(() => this.modalService.title = this.panelService.title = 'カットイン設定');
+    EventSystem.register(this)
+      .on('DELETE_GAME_OBJECT', 1000, event => {
+        if (!this.selectedCutIn || event.data.identifier !== this.selectedCutIn.identifier) return;
+        let object = ObjectStore.instance.get(event.data.identifier);
+        if (object !== null) {
+          this.selectedCutInXml = object.toXml();
+        }
+      });
+  }
+
+  ngAfterViewInit() {
+    if (this.cutIns.length > 0) {
+      setTimeout(() => {
+        this.onChangeCutIn(this.cutIns[0].identifier);
+        this.cutInSelecter.nativeElement.selectedIndex = 0;
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    EventSystem.unregister(this);
+  }
+
+  onChangeCutIn(identifier: string) {
+    this.selectedCutIn = ObjectStore.instance.get<CutIn>(identifier);
+    this.selectedCutInXml = '';
+  }
+
+  create(name: string = 'カットイン'): CutIn {
+    return CutInList.instance.addCutIn(name)
   }
 
   add() {
-    
+    const cutIn = this.create();
+    cutIn.imageIdentifier = 'stand_no_image';
+    setTimeout(() => {
+      this.onChangeCutIn(cutIn.identifier);
+      this.cutInSelecter.nativeElement.value = cutIn.identifier;
+    })
+  }
+  
+  async save() {
+    if (!this.selectedCutIn || this.isSaveing) return;
+    this.isSaveing = true;
+    this.progresPercent = 0;
+
+    let fileName: string = 'cunIn_' + this.selectedCutIn.name;
+
+    await this.saveDataService.saveGameObjectAsync(this.selectedCutIn, fileName, percent => {
+      this.progresPercent = percent;
+    });
+
+    setTimeout(() => {
+      this.isSaveing = false;
+      this.progresPercent = 0;
+    }, 500);
+  }
+
+  delete() {
+    if (!this.selectedCutIn) return;
+    EventSystem.call('STOP_CUT_IN', { 
+      identifier: this.selectedCutIn.identifier
+    });
+    if (!this.isEmpty) {
+      this.selectedCutInXml = this.selectedCutIn.toXml();
+      this.selectedCutIn.destroy();
+    }
+  }
+
+  restore() {
+    if (this.selectedCutIn && this.selectedCutInXml) {
+      let restoreCutIn = <CutIn>ObjectSerializer.instance.parseXml(this.selectedCutInXml);
+      CutInList.instance.addCutIn(restoreCutIn);
+      this.selectedCutInXml = '';
+      setTimeout(() => {
+        const cutIns = this.cutIns;
+        this.onChangeCutIn(cutIns[cutIns.length - 1].identifier);
+        this.cutInSelecter.nativeElement.selectedIndex = cutIns.length - 1;
+      });
+    }
+  }
+
+  getHidden(image: ImageFile): boolean {
+    const imageTag = ImageTag.get(image.identifier);
+    return imageTag ? imageTag.hide : false;
+  }
+
+  upTabIndex() {
+    if (!this.selectedCutIn) return;
+    let parentElement = this.selectedCutIn.parent;
+    let index: number = parentElement.children.indexOf(this.selectedCutIn);
+    if (0 < index) {
+      let prevElement = parentElement.children[index - 1];
+      parentElement.insertBefore(this.selectedCutIn, prevElement);
+    }
+  }
+
+  downTabIndex() {
+    if (!this.selectedCutIn) return;
+    let parentElement = this.selectedCutIn.parent;
+    let index: number = parentElement.children.indexOf(this.selectedCutIn);
+    if (index < parentElement.children.length - 1) {
+      let nextElement = parentElement.children[index + 1];
+      parentElement.insertBefore(nextElement, this.selectedCutIn);
+    }
+  }
+
+  openModal() {
+    if (this.isDeleted) return;
+    let currentImageIdentifires: string[] = [];
+    if (this.selectedCutIn && this.selectedCutIn.imageIdentifier) currentImageIdentifires = [this.selectedCutIn.imageIdentifier];
+    this.modalService.open<string>(FileSelecterComponent, { currentImageIdentifires: currentImageIdentifires }).then(value => {
+      if (!this.selectedCutIn || !value) return;
+      this.selectedCutIn.imageIdentifier = value;
+    });
+  }
+
+  onShowHiddenImages($event: Event) {
+    if (this.isShowHideImages) {
+      this.isShowHideImages = false;
+    } else {
+      if (window.confirm("非表示設定の画像を表示します（ネタバレなどにご注意ください）。\nよろしいですか？")) {
+        this.isShowHideImages = true;
+      } else {
+        this.isShowHideImages = false;
+        $event.preventDefault();
+      }
+    }
+  }
+  
+  helpCutIn() {
+    let coordinate = this.pointerDeviceService.pointers[0];
+    let option: PanelOption = { left: coordinate.x, top: coordinate.y, width: 600, height: 460 };
+    let textView = this.panelService.open(TextViewComponent, option);
+    textView.title = 'カットインヘルプ';
+    textView.text = 
+`　`;
+  }
+
+  playCutIn() {
+    if (!this.selectedCutIn) return;
+    EventSystem.call('PLAY_CUT_IN', { 
+      identifier: this.selectedCutIn.identifier,
+      test: false
+    });
+  }
+
+  stopCutIn() {
+    if (!this.selectedCutIn) return;
+    EventSystem.call('STOP_CUT_IN', { 
+      identifier: this.selectedCutIn.identifier
+    });
+  }
+
+  testCutIn() {
+    if (!this.selectedCutIn) return;
+    setTimeout(() => {
+      EventSystem.trigger('PLAY_CUT_IN', { 
+        identifier: this.selectedCutIn.identifier, 
+        test: true
+      });
+    });
   }
 }
