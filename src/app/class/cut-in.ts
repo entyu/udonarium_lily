f2684323858b2a4419d614d2dc8b7c29148ee4d7
DESKTOP-64JNFCF\keyra <nnanasu7@gmail.com>
2021-05-29 14:01:20 +0900

    短縮URLyoutu.be対応。

--- src/app/class/cut-in.ts
+++ src/app/class/cut-in.ts
@@ -36,9 +36,15 @@ export class CutIn extends ObjectNode {
     let ret = '';
     if (StringUtil.validUrl(this.videoUrl)) {
       const hostname = (new URL(this.videoUrl)).hostname
-      if (hostname != 'youtube.com' && hostname != 'www.youtube.com') return '';
-      let tmp = this.videoUrl.split('v=');
-      if (tmp[1]) ret = encodeURI(tmp[1].split(/[\&\#\/]/)[0]);
+      if (hostname == 'youtube.com' || hostname == 'www.youtube.com') { 
+        let tmp = this.videoUrl.split('v=');
+        if (tmp[1]) ret = encodeURI(tmp[1].split(/[\&\#\/]/)[0]);
+      } else if (hostname == 'youtu.be') {
+        let tmp = this.videoUrl.split('youtu.be/');
+        if (tmp[1]) ret = encodeURI(tmp[1].split(/[\&\#\/]/)[0]);
+      } else {
+        return '';
+      }
     } else {
       // IDだけを許可すべきか？
       return ret = '';
