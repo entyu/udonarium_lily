299593d280133d84c56539f59c82c4d54a334a0d
Hideyo Mikisato <h.mikisato@gmail.com>
2021-09-25 05:07:54 +0900

    スタイル修正

--- src/app/class/game-character.ts
+++ src/app/class/game-character.ts
@@ -61,13 +61,13 @@ export class GameCharacter extends TabletopObject {
     const iconNum = this.getIconNumElement();
     if (!iconNum) {
       const image: DataElement = this.imageDataElement.getFirstElementByName('imageIdentifier');
-      const file = ImageStorage.instance.get(<string>image.value)
+      const file = ImageStorage.instance.get(<string>image.value);
       return file ? file : ImageFile.Empty;
     } else {
       let n = <number>iconNum.currentValue;
       if (n > this.imageDataElement.children.length - 1) n = this.imageDataElement.children.length - 1;
       const image = this.imageDataElement.children[n];
-      const file = ImageStorage.instance.get(<string>image.value)
+      const file = ImageStorage.instance.get(<string>image.value);
       return file ? file : ImageFile.Empty;
     }
   }
