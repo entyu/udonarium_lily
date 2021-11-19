0749194beb11b8da320b8e77ab07a1ed4413b9d7
entyu <myuliel@hotmail.com>
2021-11-19 22:18:27 +0900

    るび側の文字条件を追加

--- src/app/component/chat-message/chat-message.component.ts
+++ src/app/component/chat-message/chat-message.component.ts
@@ -66,7 +66,7 @@ export class ChatMessageComponent implements OnInit, AfterViewInit {
     // 記入例：|永遠力暴風雪《エターナルフォースブリザード》
     // 振られる側に《スキル名》は有効：|《約束された勝利の剣》《エクスカリバー》
     let escapeText = this.escapeHtml(text);
-    return escapeText.replace(/[\|｜]([^\|｜\s]+?)《([ぁ-んァ-ヶー―・･]+?)》/g, '<ruby>$1<rt>$2</rt></ruby>');
+    return escapeText.replace(/[\|｜]([^\|｜\s]+?)《([ぁ-んァ-ヶー―・･a-zA-Zａ-ｚa-zＡ-Ｚ\!！\?？…]+?)》/g, '<ruby>$1<rt>$2</rt></ruby>');
   }
 
   escapeHtml(text) {
